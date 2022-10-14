767d43ac27d999c69cb7a7b0465f40881fbc1684
Haoyu Huang <446700679@qq.com>
2022-10-14 09:57:40 +0800

    1

--- net.py
+++ net.py
@@ -1,1 +1,24 @@
 import networkx as nx
+
+def developmental_time_window(N, k, gamma, ):
+    '''
+    implementation of developmental time window
+    to generate multiple-cluster small-world networks
+    
+    Nisbach F, Kaiser M. Developmental time windows for spatial growth generate multiple-cluster small-world networks[J]. The European Physical Journal B, 2007, 58(2): 185-191.
+    
+    N: total number of nodes in network
+    k: number of pioneer nodes
+    gamma(beta): parameter of distance function
+    
+    '''
+    G = nx.Graph()
+    
+    # add pioneer nodes to graph
+    for i in range(k):
+        G.add_node(i)
+    G
+    
+
+if __name__ == '__main__':
+    
