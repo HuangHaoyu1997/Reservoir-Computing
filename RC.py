1be6da75687e1b088d65ae50ff4ba9bec7fa025a
Haoyu Huang <446700679@qq.com>
2022-10-27 00:47:33 +0800

    1

--- RC.py
+++ RC.py
@@ -301,6 +301,29 @@ class RC:
         # y = softmax(y)
         return r, y, np.array(spike_train)
 
+
+class ConvNet(nn.Module):
+    def __init__(self, config:Config):
+        super(ConvNet, self).__init__()
+        self.layer1 = nn.Sequential(
+            nn.Conv2d(1, 16, kernel_size=5, stride=1, padding=2),
+            nn.BatchNorm2d(16),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2, stride=2))
+        self.layer2 = nn.Sequential(
+            nn.Conv2d(16, 32, kernel_size=5, stride=1, padding=2),
+            nn.BatchNorm2d(32),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2, stride=2))
+        self.fc = nn.Linear(7*7*32, config.N_hid)
+    
+    def forward(self, x):
+        out = self.layer1(x)
+        out = self.layer2(out)
+        out = out.reshape(out.size(0), -1)
+        out = self.fc(out)
+        return out
+
 class MLP(nn.Module):
     def __init__(self,
                  N_in,
--- config.py
+++ config.py
@@ -7,22 +7,22 @@ class Config:
     
     
     data = 'cifar10'                                # 'mnist'
-    train_num = 50000
+    train_num = 20000
     test_num = 10000
-    epoch = 200                                     # training epoch for readout layer mlp
-    lr = 2e-3                                       # learning rate for mlp
+    epoch = 1000                                     # training epoch for readout layer mlp
+    lr = 5e-3                                       # learning rate for mlp
     
     N_in = 32*32 if data=='cifar10' else 28*28      # input dim
-    N_hid = 500                                     # hidden dim / number of reservoir neuron
+    N_hid = 1000                                     # hidden dim / number of reservoir neuron
     N_out = 10                                      # output dim
     alpha = 0.2                                     # memory factor
 
     layer = 2         # number of reservoir layers
     p_in = 0.2        # ratio of inhibitory neurons
     gamma = 1.0       # shape factor of gamma distribution
-    frames = 30       # static img to event-based frames
-    device = 'cpu'   # 'cpu', 'cuda'
-    batch_size = 50 # batch size for inference and training
+    frames = 80       # static img to event-based frames
+    device = 'cuda'   # 'cpu', 'cuda'
+    batch_size = 500 # batch size for inference and training
     sub_thr = False   # subtract thr to mem potential when firing
     
     
@@ -38,8 +38,8 @@ class Config:
     noise = True      # add noise in A
     noise_str = 0.05  # noise strength
     p_ER = 0.2        # connection probability when creating ER graph
-    m_BA = 2          # number of edges to attach from a new node to existing nodes
-    k = 4             # number of clusters in A
+    m_BA = 3          # number of edges to attach from a new node to existing nodes
+    k = 5             # number of clusters in A
     R = 0.2           # distance factor when deciding connections in random network
     
     R_ = 1.5          # radius of all nodes in DTW algorithm
