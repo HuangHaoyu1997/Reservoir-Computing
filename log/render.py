58b2624f016b8d7c840ecca7009538af97c12c4e
Haoyu Huang <446700679@qq.com>
2022-12-06 00:59:15 +0800

    1

--- log/render.py
+++ log/render.py
@@ -1,22 +1,26 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-with open('test3.log', 'r') as f:
+with open('2022.12.05-01-33-25.log', 'r') as f:
     log = f.readlines()
 
-acc, loss = [], []
+train_acc, test_acc, train_loss, test_loss = [], [], [], []
 for i in log:
     # acc.append(float(i.split(',')[0][-6:]))
     # loss.append(float(i.split(',')[1].split('\') ')[1].split('\n')[0]))
-    acc.append(float(i.split(',')[0]))
-    loss.append(float(i.split(',')[1]))
+    train_acc.append(float(i.split(',')[0]))
+    test_acc.append(float(i.split(',')[1]))
+    train_loss.append(float(i.split(',')[2]))
+    test_loss.append(float(i.split(',')[3]))
 plt.subplot(121)
-plt.plot(acc)
-plt.axis([-5, 505, -0.05, 1.05])
+plt.plot(train_acc)
+plt.plot(test_acc)
+# plt.axis([-5, 505, -0.05, 1.05])
 plt.grid()
 
 plt.subplot(122)
-plt.plot(np.array(loss)/10)
-plt.axis([-5, 505, -0.05, 3])
+plt.plot(train_loss)
+plt.plot(test_loss)
+# plt.axis([-5, 505, -0.05, 3])
 plt.grid()
 plt.show()
